79f8ce4dfdb172634ce214aeec2a7212e7f829ae
Maverick@YesFlow <45048079+YesFlowMaverick@users.noreply.github.com>
2021-06-05 23:51:11 -0400

    Update

--- plugin-yesflow-speech/ios/Plugin/CustomSpeechView/RecorderViews.swift
+++ plugin-yesflow-speech/ios/Plugin/CustomSpeechView/RecorderViews.swift
@@ -6,19 +6,11 @@ import Capacitor
 
 @available(iOS 14.0, *)
 public extension CapacitorYesflowSpeech.RecorderViews {
-
-    
-    struct WordRow: View {
-        public var body: some View {
-            Text("TEST")
-        }
-    }
-
-    
     struct WordList : View {
         @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
         @Environment(\.swiftSpeechState) var state: CapacitorYesflowSpeech.State
         @Environment(\.actionsOnSendFinalText) var finalText: CapacitorYesflowSpeech.FinalText
+        
         
         var presentingVC: UIViewController?
         var callingPlugin: CAPPluginCall?
@@ -28,6 +20,7 @@ public extension CapacitorYesflowSpeech.RecorderViews {
         @State var list: [(session: CapacitorYesflowSpeech.Session, text: String)] = []
         @State private var text = "Hold to start speech"
         @State var activeComponent: Component? = .none
+        @State var isRecording: Bool = false
         
         var color: Color? = .blue
         
@@ -43,7 +36,7 @@ public extension CapacitorYesflowSpeech.RecorderViews {
             self.init(locale: Locale(identifier: localeIdentifier))
         }
         
-        var isRecording: Bool { state == .recording }
+        var isNotRecording: Bool { state != .recording }
         
         var isSpeechActive: Bool { activeComponent == .speech }
         var isCancelActive: Bool { activeComponent == .cancel }
@@ -122,133 +115,90 @@ public extension CapacitorYesflowSpeech.RecorderViews {
         }
 
         public var body: some View {
-            NavigationView{
-                List {
-                      ForEach(list, id: \.session.id) { pair in
-                          HStack {
-                          Text(pair.text)
-                          Spacer()
-                          }
-                      }
-                      .onDelete(perform: delete)
-                      .onMove(perform: relocate)
-                }
-                .overlay(
-                    Button(action:{ consolidateText()}) {
-                        CollapseIcon()
-                            .background(Color.blue)
-                            .foregroundColor(Color.white)
-                            .cornerRadius(40)
-                    },alignment: .bottomLeading
-                )
-                .overlay(
-                    CapacitorYesflowSpeech.RecordButton()
-                        .swiftSpeechRecordOnHold(
-                            sessionConfiguration: sessionConfiguration,
-                            animation: .spring(response: 0.3, dampingFraction: 0.5, blendDuration: 0),
-                            distanceToCancel: 100.0
-                        ).onStartRecording { session in
-                            createBlankItem(session: session)
-                        }.onCancelRecording { session in
-                            cancelLastItem(session: session)
-                        }.onRecognize(includePartialResults: true) { session, result in
-                            recognizeItem(session: session, result: result)
-                        } handleError: { session, error in
-                            errorItem(session: session, error: error)
-                        },alignment: .bottom
-                ).overlay(
-                    Button(action:{ sendText()}) {
-                        SendIcon()
-                            .background(Color.blue)
-                            .foregroundColor(Color.white)
-                            .cornerRadius(40)
+            NavigationView {
+                VStack {
+                    if list.isEmpty {
+                        List {
+            
+                        }
+                        VStack {
+                            Text("Hold the button below & speak")
+                            HintArrowView(arrowheadSize: 6)
+                                .frame(width: 200, height: 150)
+                                .foregroundColor(Color.primary)
+                        }
+                        .transition(
+                            AnyTransition.opacity.animation(Animation.linear(duration: 1))
+                        )
+                        .offset(x: 0, y: -120)
                     }
-                    ,alignment: .bottomTrailing
-                )
-                
-                .navigationBarTitle("Speech To Text",displayMode: .inline)
+
+                    if !list.isEmpty {
+                        // the same result with using List instead of ScrollView
+                        List {
+                            ForEach(list, id: \.session.id) { pair in
+                                    HStack {
+                                    Text(pair.text)
+                                    Spacer()
+                                    }
+                            }
+                            .onDelete(perform: delete)
+                            .onMove(perform: relocate)
+                        }
+                    }
+//                    HStack {
+//                        Spacer()
+//                        Text("Hold to Speak")
+//                        Spacer()
+//                    }
+                }
+                .navigationBarTitle(Text("Speech To Text"), displayMode: .inline)
                 .navigationBarItems(
                     leading: Button(action: {
                         self.cancel()
                     }, label: { Text("Cancel") }),
                     trailing: EditButton().disabled(list.isEmpty)
                 )
-                
-            }.onAppear {
-                CapacitorYesflowSpeech.requestSpeechRecognitionAuthorization()
-            }
-            
-        }
-        public var body2: some View {
-            NavigationView {
-                      List {
-                            ForEach(list, id: \.session.id) { pair in
-                                HStack {
-                                Text(pair.text)
-                                Spacer()
-                                }
-                            }.onMove(perform: relocate)
-                        .overlay(
-                            Group {
-                                HStack {
-                                    Button(action:{ undoLastItem()}) {
-                                        UndoIcon()
-                                            .background(Color.blue)
-                                            .foregroundColor(Color.white)
-                                            .cornerRadius(40)
-                                    }
-                                    Button(action:{ consolidateText()}) {
-                                        CollapseIcon()
-                                            .background(Color.blue)
-                                            .foregroundColor(Color.white)
-                                            .cornerRadius(40)
-                                    }
-                                }.padding().environment(\.colorScheme, .light)
-                            },alignment: .bottomLeading
-                        )
-                        .overlay(
-                            CapacitorYesflowSpeech.RecordButton()
-                                .swiftSpeechRecordOnHold(
-                                    sessionConfiguration: sessionConfiguration,
-                                    animation: .spring(response: 0.3, dampingFraction: 0.5, blendDuration: 0),
-                                    distanceToCancel: 100.0
-                                ).onStartRecording { session in
-                                    createBlankItem(session: session)
-                                }.onCancelRecording { session in
-                                    cancelLastItem(session: session)
-                                }.onRecognize(includePartialResults: true) { session, result in
-                                    recognizeItem(session: session, result: result)
-                                } handleError: { session, error in
-                                    errorItem(session: session, error: error)
-                                },alignment: .bottom
-                        )
-                        .overlay(
-                            Group {
-                                HStack {
-                                    Button(action:{ sendText()}) {
-                                        SendIcon()
-                                            .background(Color.blue)
-                                            .foregroundColor(Color.white)
-                                            .cornerRadius(40)
-                                    }
-                                }.padding().environment(\.colorScheme, .light)
-                            },alignment: .bottomTrailing
-                        )
-
-                      .onAppear {
-                        CapacitorYesflowSpeech.requestSpeechRecognitionAuthorization()
-                      }
+                .overlay(
+                    Button(action:{ consolidateText()}) {
+                    CollapseIcon()
+                        .background(Color.blue)
+                        .foregroundColor(Color.white)
+                        .cornerRadius(40)
+                        .padding(30)
+                    },alignment: .bottomLeading)
+                .overlay(
+                    CapacitorYesflowSpeech.RecordButton().padding(30)
+                    .swiftSpeechRecordOnHold(
+                        sessionConfiguration: sessionConfiguration,
+                        animation: .spring(response: 0.3, dampingFraction: 0.5, blendDuration: 0),
+                        distanceToCancel: 100.0
+                    ).onStartRecording { session in
+                        createBlankItem(session: session)
+                    }.onCancelRecording { session in
+                        cancelLastItem(session: session)
+                    }.onRecognize(includePartialResults: true) { session, result in
+                        recognizeItem(session: session, result: result)
+                    } handleError: { session, error in
+                        errorItem(session: session, error: error)
+                    },alignment: .bottom
+                )
+                .overlay(
+                    Button(action:{ sendText() })
+                    {
+                        SendIcon()
+                            .background(Color.blue)
+                            .foregroundColor(Color.white)
+                            .cornerRadius(40)
+                            .padding(30)
+                    }
+                    ,alignment: .bottomTrailing
+                )
+                .onAppear {
+                    CapacitorYesflowSpeech.requestSpeechRecognitionAuthorization()
                 }
-            }.navigationBarTitle("Speech to Text")
-              .navigationBarItems(leading:
-                  Button("Edit") {
-                      print("About tapped!")
-                      }
-              ).navigationBarBackButtonHidden(true)
-            
-            
+            }
         }
-      
         
         func dismiss() -> Void {
             self.presentingVC!.dismiss(animated: true)
